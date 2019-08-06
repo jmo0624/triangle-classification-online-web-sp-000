aede45fb6f0f4b736c6b32cc3f99d69f1b3decc5
Josh Morris <jmo0624@gmail.com>
2019-08-06 00:18:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -43,6 +43,10 @@ class Triangle
   def valid?
     (@side1 + @side2 > @side3) && (@side2 + @side3 > @side1) && (@side1 + @side3 > @side2)
   end
+  
+  class TriangleError < StandardError
+
+  end
 
 end
 
