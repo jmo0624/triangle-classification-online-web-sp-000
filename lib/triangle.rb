dad3a95cad139da29a14b2cfe5a3952b549d7685
Josh Morris <jmo0624@gmail.com>
2019-08-06 00:05:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,5 +2,8 @@ class Triangle
   
   def intialize(side_a, side_b, side_c)
     @triangle_sides = []
+    @triangle_sides << side_a
+    @triangle_sides << side_b
+    @triangle_sides << side_c
     
 end
